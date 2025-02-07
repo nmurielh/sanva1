dfc95896ba4565ea9812abdb9096f24b34378a66
MURIEL <125093427+nmurielh@users.noreply.github.com>
2025-02-07 10:27:14 -0500

    a

---
+++ sec/SanValentín.js
@@ -0,0 +1,6 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('¡Te Amo mi Niña hermosha <3!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('aea UU, dale que si pue :c');});
---
+++ sec/SanValentín2.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("fireworks").style.display = "none";
+    }, 4000); // 2 segundos para la animación de fuegos artificiales
+});
